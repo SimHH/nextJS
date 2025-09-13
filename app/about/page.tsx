913ed06cdf649fe37b77a68e1393092c9e083144
맥심 <macsim@maegsim-ui-MacBookAir.local>
2025-09-13 19:04:35 +0900

    Initial Next.js project

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>About 페이지</h1>
+      <p>Next.js 기초 학습 중입니다.</p>
+    </main>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+        <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+            <h1>연락처 : 010-1111-1111</h1>
+            <p>감사</p>
+        </main>
+    )
+
+}
